a961956725725028b4ecb8b1a5b5bfc6a5928d9e
chaitanyaemani <chaitanyaemani6@gmail.com>
2025-07-02 12:04:35 +0530

    Added forms

--- app/interview/page.tsx
+++ app/interview/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
 
 export default function DashboardPage() {
   return (
     <>
-      <Navbar />
+      <Sidebar />
       <div className="min-h-screen bg-gray-50 pb-20 sm:pb-6 px-6">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">Dashboard Page</h1>
         <p className="text-gray-600">This is your dashboard. Navigation works correctly now!</p>
